310d780f2335f827e25f8f69402c5bf7303b8d11
aureasiqueira1 <aurea@gec.inatel.br>
2024-02-27 17:50:01 -0300

    Adicionando tecnologias

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -3,6 +3,7 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
+import { Col, Row } from "react-bootstrap";
 
 export function ProjectCard(props) {
   return (
@@ -16,6 +17,80 @@ export function ProjectCard(props) {
 
         <Card.Text style={{ fontSize: "18px" }}>{props.description}</Card.Text>
 
+        <div className="labelGroup">
+          <Row
+            style={{
+              justifyContent: "center",
+              paddingBottom: "10px",
+            }}
+          >
+            <Col md={4}>
+              {props?.tecnology?.split(",")[0] && (
+                <div className="label">{props?.tecnology?.split(",")[0]}</div>
+              )}
+            </Col>
+
+            <Col md={4}>
+              {props?.tecnology?.split(",")[1] && (
+                <div className="label">{props?.tecnology?.split(",")[1]}</div>
+              )}
+            </Col>
+
+            <Col md={4}>
+              {props?.tecnology?.split(",")[2] && (
+                <div className="label">{props?.tecnology?.split(",")[2]}</div>
+              )}
+            </Col>
+
+            <Col md={4}>
+              {props?.tecnology?.split(",")[3] && (
+                <div className="label">{props?.tecnology?.split(",")[3]}</div>
+              )}
+            </Col>
+
+            <Col md={4}>
+              {props?.tecnology?.split(",")[4] && (
+                <div className="label">{props?.tecnology?.split(",")[4]}</div>
+              )}
+            </Col>
+
+            <Col md={4}>
+              {props?.tecnology?.split(",")[5] && (
+                <div className="label">{props?.tecnology?.split(",")[5]}</div>
+              )}
+            </Col>
+
+            <Col md={4}>
+              {props?.tecnology?.split(",")[6] && (
+                <div className="label">{props?.tecnology?.split(",")[6]}</div>
+              )}
+            </Col>
+
+            <Col md={4}>
+              {props?.tecnology?.split(",")[7] && (
+                <div className="label">{props?.tecnology?.split(",")[7]}</div>
+              )}
+            </Col>
+
+            <Col md={4}>
+              {props?.tecnology?.split(",")[8] && (
+                <div className="label">{props?.tecnology?.split(",")[8]}</div>
+              )}
+            </Col>
+
+            <Col md={4}>
+              {props?.tecnology?.split(",")[9] && (
+                <div className="label">{props?.tecnology?.split(",")[9]}</div>
+              )}
+            </Col>
+
+            <Col md={4}>
+              {props?.tecnology?.split(",")[10] && (
+                <div className="label">{props?.tecnology?.split(",")[10]}</div>
+              )}
+            </Col>
+          </Row>
+        </div>
         <div className="buttonProject">
           <Button variant="primary" href={props.ghLink} target="_blank">
             <BsGithub /> &nbsp;
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -27,113 +27,121 @@ export function Projects() {
               <Col md={4} className="project-card">
                 <ProjectCard
                   imgPath={kanban}
-                  isBlog={false}
                   title="Agenda - Kanban"
                   description="Aplicação para gerenciar tarefas de forma simpls e fácil, seja ela qual for. Inspirado no Kanban."
                   ghLink="https://github.com/aureasiqueira1/ProjectSchedule"
                   demoLink="https://project-schedule-neon.vercel.app/"
+                  tecnology={
+                    "React, JavaScript, CSS, PostgreSQL, Express, TypeORM, Postman"
+                  }
                 />
               </Col>
               <Col md={4} className="project-card">
                 <ProjectCard
                   imgPath={veículos}
-                  isBlog={false}
                   title="Veículos"
                   description="Aplicação para gerenciar carros. Permite cadastrar, editar, excluir e pesquisar um carro e simular score do usuário."
                   ghLink="https://github.com/aureasiqueira1/oncar-app"
                   demoLink="https://front-oncar.vercel.app/"
+                  tecnology={
+                    "React, TypeScript, Styled Components, NestJs, TypeORM, PostgreSQL"
+                  }
                 />
               </Col>
               <Col md={4} className="project-card">
                 <ProjectCard
                   imgPath={clientlist}
-                  isBlog={false}
                   title="Lista de Clientes"
                   description="Aplicação para gerenciar clientes. Permite cadastrar, editar, apagar cliente e classificá-lo como Vip ou Premium "
                   ghLink="https://github.com/aureasiqueira1/client-list"
                   demoLink="https://client-list-one.vercel.app/"
+                  tecnology={
+                    "React, TypeScript, Styled Components, NestJs, TypeORM, PostgreSQL, Swagger"
+                  }
                 />
               </Col>
               <Col md={4} className="project-card">
                 <ProjectCard
                   imgPath={financeiro}
-                  isBlog={false}
                   title="Sistema Financeiro"
                   description="Aplicação de um Sistema Financeiro para controlar os gastos. Permite adicionar saldos positivos e negativos."
                   ghLink="https://github.com/aureasiqueira1/App-financeiro"
                   demoLink="https://app-financeiro-nu.vercel.app/"
+                  tecnology={"React, TypeScript, Styled Components"}
                 />
               </Col>
 
               <Col md={4} className="project-card">
                 <ProjectCard
                   imgPath={metaverso}
-                  isBlog={false}
                   title="Metaverso App"
                   description="Aplicação de uma tela que permite maximizar tela, minimizar tela, fechar tela e minimizar menu."
                   ghLink="https://github.com/aureasiqueira1/metaverso-app"
                   demoLink="https://metaverso-app.vercel.app/"
+                  tecnology={"NextJS, TypeScript, Styled Components"}
                 />
               </Col>
               <Col md={4} className="project-card">
                 <ProjectCard
                   imgPath={shoppingcart}
-                  isBlog={false}
                   title="E-Commerce"
                   description="Aplicação de um E-Commerce que contém sistema de cadastro, login, carrinho de compras e favoritos."
                   ghLink="https://github.com/aureasiqueira1/Shopping"
                   demoLink="https://shopping-umber.vercel.app/"
+                  tecnology={"React, JavaScript, CSS3, HTML5, API Lorem Picsum"}
                 />
               </Col>
 
               <Col md={4} className="project-card">
                 <ProjectCard
-                  imgPath={spotify}
-                  isBlog={false}
-                  title="Spotify"
-                  description="Landing page do Spotify"
-                  ghLink="https://github.com/aureasiqueira1/landing-page-spotify"
-                  demoLink="https://landing-page-spotify-eight.vercel.app/"
-                />
-              </Col>
-              <Col md={4} className="project-card">
-                <ProjectCard
                   imgPath={quiz}
-                  isBlog={false}
                   title="Quiz"
                   description="Aplicação de um quiz com 5 perguntas de verdadeiro ou falso que englobam conhecimentos da disciplina de engenharia de software, foi desenvolvido em flutter utilizando o dart como linguagem."
                   ghLink="https://github.com/aureasiqueira1/AppQuiz"
                   demoLink="https://aureasiqueira1.github.io/AppQuiz/#/"
+                  tecnology={"Flutter, Dart, Mockito"}
                 />
               </Col>
               <Col md={4} className="project-card">
                 <ProjectCard
                   imgPath={marvel}
-                  isBlog={false}
                   title="Marvel"
                   description="Aplicação sobre a Marvel, composto por três telas: Personagens, Filmes e HQs. Aqui, é possível acessar informações detalhadas de cada seção ao clicar nelas, além de um carrossel que permite visualizar mais cards ao ser clicado."
                   ghLink="https://github.com/aureasiqueira1/marvel"
                   demoLink="https://marvel-three-sandy.vercel.app/"
+                  tecnology={"NextJs, TypeScript, Styled Components"}
                 />
               </Col>
               <Col md={4} className="project-card">
                 <ProjectCard
                   imgPath={audio}
-                  isBlog={false}
                   title="Notas"
                   description="Aplicação que grava notas utilizando áudio ou texto"
                   ghLink="https://github.com/aureasiqueira1/audio-text"
                   demoLink="https://audio-text-ruby.vercel.app/"
+                  tecnology={
+                    "NextJs, TypeScript, Tailwind, SpeechRecognition API"
+                  }
+                />
+              </Col>
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={spotify}
+                  title="Spotify"
+                  description="Landing page do Spotify"
+                  ghLink="https://github.com/aureasiqueira1/landing-page-spotify"
+                  demoLink="https://landing-page-spotify-eight.vercel.app/"
+                  tecnology={"CSS, HTML"}
                 />
               </Col>
               <Col md={4} className="project-card">
                 <ProjectCard
                   imgPath={githubsearch}
-                  isBlog={false}
                   title="Pesquisa de Repositórios"
                   description="Aplicação que faz buscas de repositórios no github pesquisando usuário/repositório e exibe as informações básicas no front-end."
                   ghLink="https://github.com/aureasiqueira1/FrontEndDesafio"
                   demoLink="https://front-end-desafio-eight.vercel.app/"
+                  tecnology={"React, TypeScript, Styled Components"}
                 />
               </Col>
             </Row>
